a5ec04a23e2014c068e0e141a8449923a03e03d8
Colin Di Meo <dimeo.colin@gmail.com>
2017-08-20 18:55:11 -0400

    feels done. may tweak a bit extra over this evening, but everything is up and working. MVP for sure. really pleased with how the style came out.

--- main.js
+++ main.js
@@ -1,12 +1,16 @@
-let submit = document.getElementById('submit');
+// setting up the search bar/search input/search button
+
+let submit = document.getElementById('submitButton');
 let url = 'https://itunes.apple.com/search?';
+
+// event listener for changing search url to user input
 
 submit.addEventListener('click', function() {
  let search = document.getElementById('search').value;
  let $request = url + 'term=' + search;
 
- console.log($request);
 
+// fetch for user search, followed by page population
 fetch($request)
  .then(
    function(response) {
@@ -22,18 +26,40 @@ fetch($request)
          let track = data.results[i].trackName;
          let artist = data.results[i].artistName;
          let image = data.results[i].artworkUrl100;
+         let preview = data.results[i].previewUrl;
          let results =
          `
+
          <img src="${image}">
-         <h4>${track}</h4>
-         <h6>${artist}</h6>
+         <h4 class="${track}">${track}</h4>
+         <h6 class="${artist}">${artist}</h6>
+
          `
-         let content = document.createElement('div');
-          content.innerHTML = results;
-          content.setAttribute('class', 'content');
-          searchResults.appendChild(content);
+
+          let content = document.createElement('span');
+           content.innerHTML = results;
+           content.setAttribute('id', `${artist}` + " - " + `"${track}"`);
+           content.setAttribute('class', `${preview}`)
+           searchResults.appendChild(content);
+
+
+             let audioClick = document.querySelectorAll('span');
+             for (let i = 0; i < audioClick.length; i++) {
+               audioClick[i].addEventListener('click', function(){
+                 let audioFile = document.getElementById('audioFile')
+                 audioFile.src = this.className;
+
+                 let audioPlayer = document.getElementById('audioPlayer');
+                   audioPlayer.load();
+
+                 let nowPlaying = document.getElementById('nowPlaying');
+                 nowPlaying.innerHTML = "Now playing: " + this.id;
+
+
+          });
+        }
       }
-   })
+   });
   }
  )
 });
